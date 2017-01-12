ac114dfd1762bb5631fe7238290b3d8b5455e8e4
Diego Pereira Grassato <dgrassato@ciandt.com>
2017-01-12 17:00:44 -0200

    [DOPS-569] Refactor demo

--- modules/hello_world/hello_world.bash
+++ modules/hello_world/hello_world.bash
@@ -2,13 +2,13 @@
 
 function hello_world_success() {
 
-  out_success "${_DEMO_TEXT}" 1
+  out_success "${_DEMO_PARAM}" 1
 
 }
 
 
 function hello_world_danger() {
 
-  out_danger "${_DEMO_TEXT}" 1
+  out_danger "${_DEMO_PARAM}" 1
 
 }
